d59bc867bc4feb384c7b94d299086c34452ea254
Tim Hagemann <tim@eternalwings.de>
2024-01-06 12:50:04 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,128 @@
+use log::{debug, info, LevelFilter};
+use retour::static_detour;
+use std::ffi::c_void;
+use std::mem;
+use windows::core::s;
+use windows::Win32::Foundation::*;
+use windows::Win32::System::Console::AllocConsole;
+use windows::Win32::System::SystemServices::*;
+use windows::Win32::System::Threading::CreateMutexA;
+
+const OPCODE_SET_FN: usize = 0x005f5b10;
+const PACKET_SEND_FN: usize = 0x00686680;
+const PACKET_WRITE_FN: usize = 0x005f7a00;
+const OPEN_AD_FN: usize = 0x00676bf0;
+
+type SetOpcodeFn = unsafe extern "thiscall" fn(*mut c_void, u32) -> *mut c_void;
+type SendPacketFn = fn(*mut c_void);
+type WriteDataFn = unsafe extern "thiscall" fn(*mut c_void, *const u8, usize);
+type OpenAdFn = unsafe extern "fastcall" fn(u32);
+
+static_detour! {
+    static SetOpcodeDetour: unsafe extern "thiscall" fn(*mut c_void, u32) -> *mut c_void;
+    static SendPacketDetour: fn(*mut c_void);
+    static WriteDetour: unsafe extern "thiscall" fn(*mut c_void, *const u8, usize);
+    static OpenAdDetour: unsafe extern "fastcall" fn(u32);
+}
+
+fn on_packet_create(this: *mut c_void, op: u32) -> *mut c_void {
+    info!("Starting packet with opcode {:x}", op);
+    debug!(
+        "Are we still in the process of sending another packet? {}",
+        WriteDetour.is_enabled()
+    );
+
+    unsafe {
+        WriteDetour.enable().expect("Should be able to enable write detour.");
+        SetOpcodeDetour.call(this, op)
+    }
+}
+
+fn on_packet_send(packet: *mut c_void) {
+    debug!("Packet has been sent.");
+    unsafe {
+        WriteDetour.disable().expect("Should be able to disable detour.");
+        SendPacketDetour.call(packet);
+    }
+}
+
+fn on_data_written(packet: *mut c_void, content: *const u8, size: usize) {
+    debug!("Writing {} bytes.", size);
+    unsafe {
+        WriteDetour.call(packet, content, size);
+    }
+}
+
+fn on_open_ad(unknown: u32) {
+    // Do nothing - we don't care.
+    return;
+}
+
+#[no_mangle]
+#[allow(non_snake_case, unused_variables)]
+extern "system" fn DllMain(dll_module: HINSTANCE, call_reason: u32, _: *mut ()) -> bool {
+    match call_reason {
+        DLL_PROCESS_ATTACH => attach(),
+        DLL_PROCESS_DETACH => detach(),
+        _ => true,
+    }
+}
+
+fn setup_logging() {
+    unsafe {
+        AllocConsole().expect("Should be able to allocation the console.");
+    }
+
+    simple_logging::log_to(std::io::stdout(), LevelFilter::Trace);
+}
+
+fn init_mutexes() {
+    unsafe {
+        CreateMutexA(None, false, s!("Silkroad Online Launcher")).expect("Should be able to create launcher mutex");
+        CreateMutexA(None, false, s!("Ready")).expect("Should be able to create ready mutex");
+    }
+    debug!("Setup mutexes to bypass launcher check.");
+}
+
+fn init_detours() {
+    unsafe {
+        let target: SetOpcodeFn = mem::transmute(OPCODE_SET_FN);
+        SetOpcodeDetour
+            .initialize(target, on_packet_create)
+            .expect("Should be able to initialize opcode detour");
+        SetOpcodeDetour
+            .enable()
+            .expect("Should be able to enable opcode detour");
+
+        let target: SendPacketFn = mem::transmute(PACKET_SEND_FN);
+        SendPacketDetour
+            .initialize(target, on_packet_send)
+            .expect("Should be able to initialize send detour");
+        SendPacketDetour.enable().expect("Should be able to enable send detour");
+
+        let target: WriteDataFn = mem::transmute(PACKET_WRITE_FN);
+        WriteDetour
+            .initialize(target, on_data_written)
+            .expect("Should be able to initialize write detour");
+
+        let target: OpenAdFn = mem::transmute(OPEN_AD_FN);
+        OpenAdDetour
+            .initialize(target, on_open_ad)
+            .expect("Should be able to initialize ad detour");
+        OpenAdDetour.enable().expect("Should be able to enable ad detour.");
+    }
+
+    info!("Detours set up, waiting for packets.");
+}
+
+fn attach() -> bool {
+    setup_logging();
+    info!("Successfully injected.");
+    init_mutexes();
+    init_detours();
+    true
+}
+
+fn detach() -> bool {
+    true
+}
---
+++ src/main.rs
@@ -0,0 +1,91 @@
+use clap::Parser;
+use dll_syringe::process::*;
+use dll_syringe::Syringe;
+use std::os::raw::c_void;
+use std::os::windows::io::FromRawHandle;
+use std::path::{Path, PathBuf};
+use windows::core::PCSTR;
+use windows::Win32::System::Threading::{
+    CreateProcessA, ResumeThread, CREATE_SUSPENDED, PROCESS_INFORMATION, STARTUPINFOA,
+};
+
+// We have to write our custom p/s macros since the once from the windows crate only accept literals.
+macro_rules! p {
+    ($string: expr) => {{
+        let s = std::ffi::CString::new($string).unwrap();
+        windows::core::PSTR::from_raw(s.into_raw() as *mut u8)
+    }};
+}
+
+macro_rules! s {
+    ($string: expr) => {{
+        let s = std::ffi::CString::new($string).unwrap();
+        windows::core::PCSTR::from_raw(s.into_raw() as *mut u8)
+    }};
+}
+
+fn create_suspended_process(exe_path: &Path, args: &str) -> (OwnedProcess, PROCESS_INFORMATION) {
+    let startupinfoa = STARTUPINFOA { ..Default::default() };
+    let mut information = PROCESS_INFORMATION { ..Default::default() };
+    let parent_dir = exe_path.parent().expect("Should be able to get parent for path");
+    let file = exe_path
+        .file_name()
+        .and_then(|f| f.to_str())
+        .unwrap_or("sro_client.exe");
+    let parent_dir_string = parent_dir
+        .as_os_str()
+        .to_str()
+        .expect("Should be able to resolve parent dir as string.");
+    let current_dir = s!(parent_dir_string);
+
+    unsafe {
+        CreateProcessA(
+            PCSTR::null(),
+            p!(format!("{} {}", file, args)),
+            None,
+            None,
+            false,
+            CREATE_SUSPENDED,
+            None,
+            current_dir,
+            &startupinfoa,
+            &mut information,
+        )
+        .expect("Process should be created");
+
+        (
+            // Unfortunately the handle required here is a winapi HANDLE, while we have a
+            // windows-rs HANDLE and thus incompatible.
+            OwnedProcess::from_raw_handle(information.hProcess.0 as *mut c_void),
+            information,
+        )
+    }
+}
+
+#[derive(Parser)]
+struct Cli {
+    silkroad: Option<PathBuf>,
+    dll: Option<PathBuf>,
+}
+
+fn main() {
+    let cli_args = Cli::parse();
+    let silkroad_path = cli_args
+        .silkroad
+        .unwrap_or_else(|| PathBuf::from("C:/Program Files (x86)/Silkroad/sro_client.exe"));
+    let (process, info) = create_suspended_process(&silkroad_path, "0 /18 0 0 0");
+
+    unsafe {
+        let syringe = Syringe::for_process(process);
+        if let Err(e) = syringe.inject(
+            cli_args
+                .dll
+                .unwrap_or_else(|| PathBuf::from("C:\\Program Files (x86)\\Silkroad\\pocket.dll")),
+        ) {
+            eprintln!("{:?}", e);
+        }
+
+        ResumeThread(info.hThread);
+        ResumeThread(info.hProcess);
+    }
+}
