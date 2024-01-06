ab395b8983ccbbe5d4653d0d5ed026c9b8172112
Tim Hagemann <tim@eternalwings.de>
2024-01-06 20:35:18 +0100

    Recorder packet data and its size when its being sent
    One of the main reasons to hook into the client is that we can figure out which
    bytes of the packet were submitted together. This allows us to easily find the
    size of a given element. With this, we now write down this information by
    grouping the sent bytes according to the size the client puts in.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use log::{debug, info, LevelFilter};
+use once_cell::sync::OnceCell;
 use retour::static_detour;
 use std::ffi::c_void;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use std::path::Path;
+use std::sync::Mutex;
 use std::{env, mem};
 use windows::core::s;
 use windows::Win32::Foundation::*;
@@ -8,34 +13,131 @@ use windows::Win32::System::Console::AllocConsole;
 use windows::Win32::System::SystemServices::*;
 use windows::Win32::System::Threading::CreateMutexA;
 
-const OPCODE_SET_FN: usize = 0x005f5b10;
+const OPCODE_SET_FN: usize = 0x00686300;
 const PACKET_SEND_FN: usize = 0x00686680;
 const PACKET_WRITE_FN: usize = 0x005f7a00;
 const OPEN_AD_FN: usize = 0x00676bf0;
 
-type SetOpcodeFn = unsafe extern "thiscall" fn(*mut c_void, u32) -> *mut c_void;
+type SetOpcodeFn = fn(u16, u32);
 type SendPacketFn = fn(*mut c_void);
-type WriteDataFn = unsafe extern "thiscall" fn(*mut c_void, *const u8, usize);
+type WriteDataFn = unsafe extern "thiscall" fn(*mut c_void, *const u32, usize);
 type OpenAdFn = unsafe extern "fastcall" fn(u32);
 
 static_detour! {
-    static SetOpcodeDetour: unsafe extern "thiscall" fn(*mut c_void, u32) -> *mut c_void;
+    static SetOpcodeDetour: fn(u16, u32);
     static SendPacketDetour: fn(*mut c_void);
-    static WriteDetour: unsafe extern "thiscall" fn(*mut c_void, *const u8, usize);
+    static WriteDetour: unsafe extern "thiscall" fn(*mut c_void, *const u32, usize);
     static OpenAdDetour: unsafe extern "fastcall" fn(u32);
 }
 
-fn on_packet_create(this: *mut c_void, op: u32) -> *mut c_void {
-    info!("Starting packet with opcode {:x}", op);
-    debug!(
-        "Are we still in the process of sending another packet? {}",
-        WriteDetour.is_enabled()
-    );
+static PACKET_RECORDER: OnceCell<Mutex<PacketRecorder>> = OnceCell::new();
 
-    unsafe {
-        WriteDetour.enable().expect("Should be able to enable write detour.");
-        SetOpcodeDetour.call(this, op)
+struct PacketInProcess {
+    opcode: u16,
+    data: Vec<Vec<u8>>,
+}
+
+impl PacketInProcess {
+    pub fn new(opcode: u16) -> Self {
+        Self {
+            opcode,
+            data: Vec::new(),
+        }
     }
+
+    fn record(&mut self, data: Vec<u8>) {
+        self.data.push(data);
+    }
+}
+
+struct PacketRecorder {
+    file: File,
+    current_packet: Option<PacketInProcess>,
+}
+
+impl PacketRecorder {
+    fn start_packet(&mut self, opcode: u16) {
+        if self.current_packet.is_none() {
+            self.current_packet = Some(PacketInProcess::new(opcode));
+        }
+    }
+
+    fn record_data(&mut self, data: Vec<u8>) {
+        if let Some(packet) = self.current_packet.as_mut() {
+            packet.record(data);
+        }
+    }
+
+    fn finish_current_packet(&mut self) {
+        if let Some(packet) = self.current_packet.take() {
+            self.write_packet(packet)
+                .expect("Should be able to write packet to file");
+        }
+    }
+
+    fn write_packet(&mut self, packet: PacketInProcess) -> std::io::Result<()> {
+        self.file.write(format!("{:x}", packet.opcode).as_bytes())?;
+        self.file.write(b"\t")?;
+        for data in packet.data {
+            self.file.write(
+                format!(
+                    "[{}]",
+                    data.iter().map(|b| format!("{:x}", b)).collect::<Vec<_>>().join(" ")
+                )
+                .as_bytes(),
+            )?;
+        }
+        self.file.write(b"\n")?;
+        self.file.flush()?;
+        Ok(())
+    }
+}
+
+fn init_recorder() -> Mutex<PacketRecorder> {
+    let target_path = Path::new("packets.txt");
+
+    let f = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(target_path)
+        .expect("Should be able to open file");
+
+    let recorder = PacketRecorder {
+        file: f,
+        current_packet: None,
+    };
+
+    Mutex::new(recorder)
+}
+
+fn on_packet_create(opcode: u16, is_finished: u32) {
+    if is_finished == 0 {
+        info!("Starting packet with opcode {:x}", opcode);
+        debug!(
+            "Are we still in the process of sending another packet? {}",
+            WriteDetour.is_enabled()
+        );
+        PACKET_RECORDER
+            .get_or_init(init_recorder)
+            .lock()
+            .expect("Should be able to lock packet recorder")
+            .start_packet(opcode);
+        unsafe {
+            WriteDetour.enable().expect("Should be able to enable write detour.");
+        }
+    } else {
+        info!("Finished packet with opcode {:x}", opcode);
+        PACKET_RECORDER
+            .get_or_init(init_recorder)
+            .lock()
+            .expect("Should be able to lock packet recorder")
+            .finish_current_packet();
+        unsafe {
+            WriteDetour.disable().expect("Should be able to enable write detour.");
+        }
+    }
+
+    SetOpcodeDetour.call(opcode, is_finished)
 }
 
 fn on_packet_send(packet: *mut c_void) {
@@ -46,8 +148,20 @@ fn on_packet_send(packet: *mut c_void) {
     }
 }
 
-fn on_data_written(packet: *mut c_void, content: *const u8, size: usize) {
+fn on_data_written(packet: *mut c_void, content: *const u32, size: usize) {
     debug!("Writing {} bytes.", size);
+
+    let mut target = vec![0u8; size];
+    unsafe {
+        std::ptr::copy_nonoverlapping(mem::transmute(content), target.as_mut_ptr(), size);
+    }
+
+    PACKET_RECORDER
+        .get_or_init(init_recorder)
+        .lock()
+        .expect("Should be able to lock packet recorder")
+        .record_data(target);
+
     unsafe {
         WriteDetour.call(packet, content, size);
     }
@@ -89,38 +203,52 @@ fn should_skip_ad() -> bool {
 
 fn init_detours() {
     unsafe {
-        let target: SetOpcodeFn = mem::transmute(OPCODE_SET_FN);
-        SetOpcodeDetour
-            .initialize(target, on_packet_create)
-            .expect("Should be able to initialize opcode detour");
-        SetOpcodeDetour
-            .enable()
-            .expect("Should be able to enable opcode detour");
-
-        let target: SendPacketFn = mem::transmute(PACKET_SEND_FN);
-        SendPacketDetour
-            .initialize(target, on_packet_send)
-            .expect("Should be able to initialize send detour");
-        SendPacketDetour.enable().expect("Should be able to enable send detour");
-
-        let target: WriteDataFn = mem::transmute(PACKET_WRITE_FN);
-        WriteDetour
-            .initialize(target, on_data_written)
-            .expect("Should be able to initialize write detour");
-
+        init_opcode_set_detour();
+        // init_packet_send_detour();
+        init_packet_write_detour();
         if should_skip_ad() {
-            let target: OpenAdFn = mem::transmute(OPEN_AD_FN);
-            OpenAdDetour
-                .initialize(target, on_open_ad)
-                .expect("Should be able to initialize ad detour");
-            OpenAdDetour.enable().expect("Should be able to enable ad detour.");
+            init_skip_ad_detour();
         }
     }
+}
+
+unsafe fn init_skip_ad_detour() {
+    let target: OpenAdFn = mem::transmute(OPEN_AD_FN);
+    OpenAdDetour
+        .initialize(target, on_open_ad)
+        .expect("Should be able to initialize ad detour");
+    OpenAdDetour.enable().expect("Should be able to enable ad detour.");
+}
+
+unsafe fn init_packet_write_detour() {
+    let target: WriteDataFn = mem::transmute(PACKET_WRITE_FN);
+    WriteDetour
+        .initialize(target, on_data_written)
+        .expect("Should be able to initialize write detour");
+}
+
+unsafe fn init_packet_send_detour() {
+    let target: SendPacketFn = mem::transmute(PACKET_SEND_FN);
+    SendPacketDetour
+        .initialize(target, on_packet_send)
+        .expect("Should be able to initialize send detour");
+    SendPacketDetour.enable().expect("Should be able to enable send detour");
+}
+
+unsafe fn init_opcode_set_detour() {
+    let target: SetOpcodeFn = mem::transmute(OPCODE_SET_FN);
+    SetOpcodeDetour
+        .initialize(target, on_packet_create)
+        .expect("Should be able to initialize opcode detour");
+    SetOpcodeDetour
+        .enable()
+        .expect("Should be able to enable opcode detour");
 }
 
 fn attach() -> bool {
     setup_logging();
     info!("Successfully injected.");
+    init_recorder();
     init_mutexes();
     init_detours();
     info!("Detours set up, waiting for packets.");
